9afd7ddfaa2dbfc908897cc3fd78572cd07aa1d6
NRAJKU <neeraj.kumar@thoughtworks.com>
2021-09-15 09:24:39 +0530

    PORT fix

--- src/main/java/com/example/simplemicroservice/SimpleMicroserviceApplication.java
+++ src/main/java/com/example/simplemicroservice/SimpleMicroserviceApplication.java
@@ -13,7 +13,7 @@ public class SimpleMicroserviceApplication {
         SpringApplication.run(SimpleMicroserviceApplication.class, args);
     }
 
-    @GetMapping("/api/v1/helo")
+    @GetMapping("/api/v1/hello")
     public String helloWorls(){
         return "Hello World!! Awesome";
     }
